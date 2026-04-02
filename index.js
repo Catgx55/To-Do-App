b8428916a985c6888552a934722088e8e694edee
Cesar Trujillo <catgx55@gmail.com>
2026-04-02 00:47:42 -0500

    implementacion de localStorage

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+// 1. al cargar la pagina, intentamos recuperar las tareas guardadas
+let tareas = JSON.parse(localStorage.getItem('misTareas')) || [];
+renderizarTareas(); // Dibujamos lo que se haya guardado
+
 function addTask() {
     const input = document.getElementById('taskInput');
-    const list = document.getElementById('taskList');
-
+    
     if (input.value.trim() !== "") {
-        // crea el elemento de la lista (li)
-        const li = document.createElement('li')
-        li.innerHTML = `${input.value}
-        <button class="delete-btn" onclick="this.parentElement.remove()">X</button>`;
-
-        // agregar a la lista visual
-        list.appendChild(li);
-
+        // 2. Agregamos la nueva tarea al arreglo
+        tareas.push(input.value);
+        // 3. Guardamos el arreglo actualizado en el localstorage
+        actualizarStorage();
+        
         // limpiar el cuadro de texto
         input.value = "";
+        renderizarTareas();
     }
+}
+
+function actualizarStorage(){
+    localStorage.setItem('misTareas', JSON.stringify(tareas));
+}
+
+function renderizarTareas(){
+    const list = document.getElementById('taskList');
+    list.innerHTML = ""; // limpiamos la lista
+
+    tareas.forEach((tareas, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        ${tareas}
+        <button class="delete-btn" onclick="eliminarTarea(${index})">X</button>`;
+        list.appendChild(li);
+    });
+}
+
+function eliminarTarea(index) {
+    //4. Quitamos la tarea del arreglo, actualizamos localStorage
+    tareas.splice(index, 1);
+    actualizarStorage();
+    renderizarTareas();
 }

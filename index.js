36eab792f5a11b10774fb20a67007ef7bdc766eb
Cesar Trujillo <catgx55@gmail.com>
2026-04-06 22:33:31 -0500

    Actualizacion de eliminar, completar, agregar tarea.

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ renderizarTareas(); // Dibujamos lo que se haya guardado
 function addTask() {
     const input = document.getElementById('taskInput');
     
+    
     if (input.value.trim() !== "") {
+        const nuevaTarea = {
+            id: Date.now(), // Se genera un ID
+            texto: input.value,
+            completada: false
+        };
         // 2. Agregamos la nueva tarea al arreglo
-        tareas.push(input.value);
+        console.log(nuevaTarea)
+        tareas.push(nuevaTarea);
+        //tareas.push(input.value);
         // 3. Guardamos el arreglo actualizado en el localstorage
         actualizarStorage();
-        
         // limpiar el cuadro de texto
         input.value = "";
         renderizarTareas();
@@ -23,20 +30,46 @@ function actualizarStorage(){
 
 function renderizarTareas(){
     const list = document.getElementById('taskList');
+    const counterElement = document.getElementById('taskCounter');
     list.innerHTML = ""; // limpiamos la lista
 
-    tareas.forEach((tareas, index) => {
+    tareas.forEach((tarea) => {
         const li = document.createElement('li');
+        const estiloTachado = tarea.completada ? 'text-decoration: line-through; color: gray;' : '';
         li.innerHTML = `
-        ${tareas}
-        <button class="delete-btn" onclick="eliminarTarea(${index})">X</button>`;
+        <span style="${estiloTachado}" onclick="toggleTarea(${tarea.id})">
+        ${tarea.texto}
+        </span>
+        <button class="delete-btn" onclick="eliminarTarea(${tarea.id})">X</button>`;
         list.appendChild(li);
     });
+
+    const pendientes = tareas.filter(t => !t.completada).length;
+
+    if(tareas.length === 0) {
+        counterElement.innerText = "No tienes tareas, puedes descansar";
+    } else {
+        counterElement.innerText = `Tareas pendientes ${pendientes}`;
+    }
 }
 
-function eliminarTarea(index) {
-    //4. Quitamos la tarea del arreglo, actualizamos localStorage
-    tareas.splice(index, 1);
+function toggleTarea(id) {
+    // 1. buscamos la tarea por el ID
+    const tareaEncontrada = tareas.find(t => t.id === id);
+
+    if(tareaEncontrada) {
+        // 2. aca cambiamos el estado de true a false o de false a true
+        tareaEncontrada.completada = !tareaEncontrada.completada;
+        // 3. guardamos el nuevo estado
+        actualizarStorage();
+        renderizarTareas();
+    }
+}
+
+function eliminarTarea(id) {
+    // Quitamos la tarea del arreglo, actualizamos localStorage
+    tareas = tareas.filter(t => Number(t.id) !== Number(id));
+    // se guarda el nuevo arreglo sin la tareas borradas
     actualizarStorage();
     renderizarTareas();
 }
